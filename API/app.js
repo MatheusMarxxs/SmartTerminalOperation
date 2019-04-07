4f33289365703ff953dcaf32f871866f762290be
unknown <matheus@marxxs.com.br>
2019-04-07 01:02:58 -0300

    app.js cors

--- API/app.js
+++ API/app.js
@@ -20,6 +20,25 @@ var indexRoutesAtracacao = require("./routes/atracacao.route");
 var routesEscala = require("./routes/escalas.route");
 // var APIUsuarios     = require("./routes/API/usuarios");
 
+app.use(function (req, res, next) {
+
+  // Website you wish to allow to connect
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4200');
+
+  // Request methods you wish to allow
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+  // Request headers you wish to allow
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader('Access-Control-Allow-Credentials', true);
+
+  // Pass to next layer of middleware
+  next();
+});
+
 // Configuração das Rotas
 app.use("/navio", indexRoutes);     // Rotas principais
 app.use("/evento", indexRoutesEvento);     // Rotas principais
