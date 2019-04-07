2a13c52fec20c99b86e1acf4b8241a71e209b43c
unknown <matheus@marxxs.com.br>
2019-04-06 23:21:41 -0300

    ..

--- FRONT/web/src/app/app-routing.module.ts
+++ FRONT/web/src/app/app-routing.module.ts
@@ -1,11 +1,8 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { NavioComponent } from './navio/navio.component';
-<<<<<<< HEAD
 import { EscalasComponent } from './escalas/escalas.component';
-=======
 import { EventoComponent } from './evento/evento.component';
->>>>>>> 972147c86e1fea1f46ca60867b61c8a4026b4e6c
 
 
 const routes: Routes = [
@@ -13,15 +10,13 @@ const routes: Routes = [
     path: 'navio',
     component: NavioComponent
   },
-<<<<<<< HEAD
   {
     path: 'escalas',
     component: EscalasComponent
-=======
+  },
   { 
     path: 'evento',
     component: EventoComponent
->>>>>>> 972147c86e1fea1f46ca60867b61c8a4026b4e6c
   }
 ];
 
