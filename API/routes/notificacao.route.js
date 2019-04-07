3e60feadeee5ca57887b78ca47235f6c257994f3
unknown <matheus@marxxs.com.br>
2019-04-06 21:48:26 -0300

    .TXT

--- API/routes/notificacao.route.js
+++ API/routes/notificacao.route.js
@@ -26,6 +26,7 @@ router.post("/", function(req, res) {
 
     notificacao.save().then(nofificacao => {
 
+        
         var jsonPayload = JSON.stringify({
             //group_admin: "5513991678484", // TODO: Specify the WhatsApp number of the group creator, including the country code
             group_admin: "5513992019511", // TODO: Specify the WhatsApp number of the group creator, including the country code
