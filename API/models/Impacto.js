d60a8a6663c27a5d09b9fbf7482ff78797fc3468
rvazquez <rvf.vazquez@hotmail.com>
2019-04-07 00:04:17 -0300

    impacto

--- API/models/Impacto.js
+++ API/models/Impacto.js
@@ -11,33 +11,53 @@ let Impacto = new mongoose.Schema({
     NAVIO:String ,
     VIAGEM:String,
     TIPO_EQUIPAMENTO: String,
-    CODIGO_EQUIPAMENTO: Number,
-    OPERADORES_IMPACTADOS: [OperadorImpactado],
-    ESCALAS_IMPACTADAS: [EscalaImpactada],
+    CODIGO_EQUIPAMENTO: Number //,
+    // OPERADORES_IMPACTADOS: [
+    //                         {
+    //                             CEP: String,
+    //                             ESCALA: String,
+    //                             TURMA: Number,
+    //                             CARGO: String
+    //                         }
+    //                         ],
+    // ESCALAS_IMPACTADAS: [{
+    //     CHEGADA_BARRA: Date,
+    //     PREVISAO_ATRACACAO: Date,
+    //     ESCA_DTHR_ATRACACAO: Date,
+    //     INICIO_OPERACAO: Date,
+    //     FIM_OPERCACAO: Date,
+    //     DESATRACACAO: Date,
+    //     NAVI_NOME: String,
+    //     HINA_QTDE_TOTAL_MOV: Number,
+    //     HINA_CMPH: String,
+    //     SERVICO: String,
+    //     LINER: String,
+    //     BERCO_ATRACACAO: String
+    // }]
 
 });
 
-let OperadorImpactado = new mongoose.Schema({
-    CEP:String,
-    ESCALA: String,
-    TURMA: Number,
-    CARGO: String
-});
+// let OperadorImpactado = new mongoose.Schema({
+//     CEP:String,
+//     ESCALA: String,
+//     TURMA: Number,
+//     CARGO: String
+// });
 
-let EscalaImpactada = new mongoose.Schema({   
-    CHEGADA_BARRA: Date,
-    PREVISAO_ATRACACAO: Date,
-    ESCA_DTHR_ATRACACAO: Date,
-    INICIO_OPERACAO: Date,
-    FIM_OPERCACAO: Date,
-    DESATRACACAO: Date,
-    NAVI_NOME: String,
-    HINA_QTDE_TOTAL_MOV: Number,
-    HINA_CMPH: String,
-    SERVICO: String,
-    LINER: String,
-    BERCO_ATRACACAO: String
-});
+// let EscalaImpactada = new mongoose.Schema({   
+//     CHEGADA_BARRA: Date,
+//     PREVISAO_ATRACACAO: Date,
+//     ESCA_DTHR_ATRACACAO: Date,
+//     INICIO_OPERACAO: Date,
+//     FIM_OPERCACAO: Date,
+//     DESATRACACAO: Date,
+//     NAVI_NOME: String,
+//     HINA_QTDE_TOTAL_MOV: Number,
+//     HINA_CMPH: String,
+//     SERVICO: String,
+//     LINER: String,
+//     BERCO_ATRACACAO: String
+// });
 
 
 module.exports = mongoose.model("Impacto", Impacto);
--- API/routes/EventoImpactoOperacao.route.js
+++ API/routes/EventoImpactoOperacao.route.js
@@ -2,6 +2,7 @@ var express = require("express");
 var router  = express.Router();
 
 var EventoImpactoOperacao = require("../models/EventoImpactoOperacao");
+var ImpactoOperacao = require("../models/Impacto");
 
 router.get("/", function(req, res) {
     EventoImpactoOperacao.find(function (err, eventos) {
@@ -17,7 +18,7 @@ router.post("/", function(req, res) {
     let eventoImpactoOperacao = new EventoImpactoOperacao(req.body);
 
     eventoImpactoOperacao.save().then(eventoImpactoOperacao => {
-    AplicarImpactos(eventoImpactoOperacao);
+        AplicarImpactos(eventoImpactoOperacao);
         
         res.status(200).json({ 'eventoImpactoOperacao': 'eventoImpactoOperacao adicionado com sucesso'})
     })
@@ -26,17 +27,58 @@ router.post("/", function(req, res) {
     });
 }); 
 
-AplicarImpactos(evento)
+function AplicarImpactos(evento)
 {
+    console.log(evento);
+    let impacto = new ImpactoOperacao();
 
-    let impacto = new Impacto();
+    impacto.TIPO        = evento.TIPO;
+    impacto.NOME_EVENTO = evento.NOME_EVENTO;
+    impacto.DATA_OCORRENCIA_EVENTO = evento.DATA_OCORRENCIA_EVENTO;
+    impacto.CAUSA_EVENTO     = evento.CAUSA_EVENTO
+    impacto.NAVIO = evento.NAVIO;
+    impacto.VIAGEM = evento.VIAGEM;
+    impacto.TIPO_EQUIPAMENTO = evento.TIPO_EQUIPAMENTO;
+    impacto.CODIGO_EQUIPAMENTO =  evento.CODIGO_EQUIPAMENTO;
+
     impacto.DATA_INICIAL = '2019-04-06';
     impacto.DATA_FINAL_PREVISTA = '2019-04-06';
-    impacto.DATA_FINAL= '2019-04-06';
+    impacto.DATA_FINAL = '2019-04-06';
+    
+    // var OperadorImpactado = new OperadorImpactado(
+    //     {
+    //         CEP: "123",
+    //         ESCALA: "26",
+    //         TURMA: "15",
+    //         CARGO: "Operador1"
+    //     }
+    // );
+
+    // impacto.OPERADORES_IMPACTADOS = [OperadorImpactado]; 
+
+    // var EscalaImpactada = new EscalaImpactada(
+    //     {
+    //         CHEGADA_BARRA: "2019-04-03T10:00:00",
+    //         PREVISAO_ATRACACAO: "2019-04-03T10:00:00",
+    //         ESCA_DTHR_ATRACACAO: "2019-04-03T10:00:00",
+    //         INICIO_OPERACAO: "2019-04-03T10:00:00",
+    //         FIM_OPERCACAO: "2019-04-03T10:00:00",
+    //         DESATRACACAO: "2019-04-03T10:00:00",
+    //         NAVI_NOME: "MSC XPTO",
+    //         HINA_QTDE_TOTAL_MOV: "12",
+    //         HINA_CMPH: "23",
+    //         SERVICO: "123",
+    //         LINER: "234",
+    //         BERCO_ATRACACAO: "333"
+    //     }
+    // );
+
+    // impacto.ESCALAS_IMPACTADAS = [EscalaImpactada];
+
     impacto.save().then(eventoImpactoOperacao => {            
         })
         .catch(err => {
-            res.status(400).send("erro ao salvar");
+            res.status(400).send("erro ao salvar" + err );
         });
 
 }
