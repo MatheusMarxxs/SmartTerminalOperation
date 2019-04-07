80c6eea95bcd9f64d22864f16f508e618d9ef738
Felipe Gonzalez <felipebgonzalez@hotmail.com>
2019-04-06 23:06:23 -0300

    ajustes

--- API/models/Impacto.js
+++ API/models/Impacto.js
@@ -11,7 +11,33 @@ let Impacto = new mongoose.Schema({
     NAVIO:String ,
     VIAGEM:String,
     TIPO_EQUIPAMENTO: String,
-    CODIGO_EQUIPAMENTO: Number
+    CODIGO_EQUIPAMENTO: Number,
+    OPERADORES_IMPACTADOS: [OperadorImpactado],
+    ESCALAS_IMPACTADAS: [EscalaImpactada],
+
 });
+
+let OperadorImpactado = new mongoose.Schema({
+    CEP:String,
+    ESCALA: String,
+    TURMA: Number,
+    CARGO: String
+});
+
+let EscalaImpactada = new mongoose.Schema({   
+    CHEGADA_BARRA: Date,
+    PREVISAO_ATRACACAO: Date,
+    ESCA_DTHR_ATRACACAO: Date,
+    INICIO_OPERACAO: Date,
+    FIM_OPERCACAO: Date,
+    DESATRACACAO: Date,
+    NAVI_NOME: String,
+    HINA_QTDE_TOTAL_MOV: Number,
+    HINA_CMPH: String,
+    SERVICO: String,
+    LINER: String,
+    BERCO_ATRACACAO: String
+});
+
 
 module.exports = mongoose.model("Impacto", Impacto);
--- API/routes/EventoImpactoOperacao.route.js
+++ API/routes/EventoImpactoOperacao.route.js
@@ -17,7 +17,7 @@ router.post("/", function(req, res) {
     let eventoImpactoOperacao = new EventoImpactoOperacao(req.body);
 
     eventoImpactoOperacao.save().then(eventoImpactoOperacao => {
-
+    AplicarImpactos(eventoImpactoOperacao);
         
         res.status(200).json({ 'eventoImpactoOperacao': 'eventoImpactoOperacao adicionado com sucesso'})
     })
@@ -25,5 +25,25 @@ router.post("/", function(req, res) {
         res.status(400).send("erro ao salvar");
     });
 }); 
+
+AplicarImpactos(evento)
+{
+
+    let impacto = new Impacto();
+    impacto.DATA_INICIAL = '2019-04-06';
+    impacto.DATA_FINAL_PREVISTA = '2019-04-06';
+    impacto.DATA_FINAL= '2019-04-06';
+    impacto.save().then(eventoImpactoOperacao => {            
+        })
+        .catch(err => {
+            res.status(400).send("erro ao salvar");
+        });
+
+}
+
+
+
+
+
 
 module.exports = router;
