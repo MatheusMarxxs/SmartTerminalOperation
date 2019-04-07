5819bdb96fcac513d1871cdd2146fbe0f8f60d9e
rvazquez <rvf.vazquez@hotmail.com>
2019-04-07 00:26:37 -0300

    impacto objeto agregado

--- API/routes/EventoImpactoOperacao.route.js
+++ API/routes/EventoImpactoOperacao.route.js
@@ -45,35 +45,33 @@ function AplicarImpactos(evento)
     impacto.DATA_FINAL_PREVISTA = '2019-04-06';
     impacto.DATA_FINAL = '2019-04-06';
     
-    // var OperadorImpactado = new OperadorImpactado(
-    //     {
-    //         CEP: "123",
-    //         ESCALA: "26",
-    //         TURMA: "15",
-    //         CARGO: "Operador1"
-    //     }
-    // );
+    var OperadorImpactado =  {
+            CEP: "123",
+            ESCALA: "26",
+            TURMA: "15",
+            CARGO: "Operador1"
+        }
+    
 
-    // impacto.OPERADORES_IMPACTADOS = [OperadorImpactado]; 
+    
 
-    // var EscalaImpactada = new EscalaImpactada(
-    //     {
-    //         CHEGADA_BARRA: "2019-04-03T10:00:00",
-    //         PREVISAO_ATRACACAO: "2019-04-03T10:00:00",
-    //         ESCA_DTHR_ATRACACAO: "2019-04-03T10:00:00",
-    //         INICIO_OPERACAO: "2019-04-03T10:00:00",
-    //         FIM_OPERCACAO: "2019-04-03T10:00:00",
-    //         DESATRACACAO: "2019-04-03T10:00:00",
-    //         NAVI_NOME: "MSC XPTO",
-    //         HINA_QTDE_TOTAL_MOV: "12",
-    //         HINA_CMPH: "23",
-    //         SERVICO: "123",
-    //         LINER: "234",
-    //         BERCO_ATRACACAO: "333"
-    //     }
-    // );
-
-    // impacto.ESCALAS_IMPACTADAS = [EscalaImpactada];
+    var EscalaImpactada = {
+            CHEGADA_BARRA: "2019-04-03T10:00:00",
+            PREVISAO_ATRACACAO: "2019-04-03T10:00:00",
+            ESCA_DTHR_ATRACACAO: "2019-04-03T10:00:00",
+            INICIO_OPERACAO: "2019-04-03T10:00:00",
+            FIM_OPERCACAO: "2019-04-03T10:00:00",
+            DESATRACACAO: "2019-04-03T10:00:00",
+            NAVI_NOME: "MSC XPTO",
+            HINA_QTDE_TOTAL_MOV: "12",
+            HINA_CMPH: "23",
+            SERVICO: "123",
+            LINER: "234",
+            BERCO_ATRACACAO: "333"
+        }
+    
+    impacto.OPERADORES_IMPACTADOS.push(OperadorImpactado); 
+    impacto.ESCALAS_IMPACTADAS.push(EscalaImpactada);
 
     impacto.save().then(eventoImpactoOperacao => {            
         })
